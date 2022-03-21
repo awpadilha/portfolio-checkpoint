8f7e83f62b4085802dd4720806922445e18fa2b8
André Padilha <awpadilha.dev@gmail.com>
2022-03-20 23:24:42 -0300

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,12 @@
+import "./styles.css"
+
+
+export default function About() {
+    return(
+        <main id="about">
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean viverra, neque eget viverra pellentesque, elit lectus vulputate nunc, quis consectetur elit tellus sed massa. Curabitur pharetra sed leo sed posuere. Nullam non ex sit amet ex auctor mollis ac ut orci. Proin eu sollicitudin sem, ac aliquam ex. Sed ut nisl sit amet tortor lacinia consectetur. Curabitur convallis sapien tellus, sit amet auctor dolor egestas quis. Suspendisse potenti. Vivamus tempus facilisis orci, sed ultricies tellus mollis id. Suspendisse potenti. Donec ac sodales felis, at eleifend quam. Nunc gravida ligula at sagittis placerat. Mauris quis sem eu metus pellentesque tincidunt vel eu justo.
+            </p>
+        </main>
+    )
+}
---
+++ src/components/Social/index.js
@@ -0,0 +1,20 @@
+import  {FiInstagram} from "react-icons/fi";
+import  {BsLinkedin, BsFillBasketFill} from "react-icons/bs";
+
+export default function Social() {
+    return(
+        <div style={{display: "flex", 
+        justifyContent: "center", 
+        alignItems: "center",
+        padding: 30,
+        }}
+        >
+            <a href="http://www.instagram.com.br">
+            <FiInstagram size={60} color="purple"/>
+            </a>
+            
+            <BsLinkedin size={60} color="blue"/>
+            <BsFillBasketFill size={60} />
+        </div>
+    )
+}
